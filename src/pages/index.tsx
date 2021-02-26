a6534f6aef57a66a52dff381d9eb6e1de478779f
Viviane Gomes Araujo <viviane.sp13@gmail.com>
2021-02-26 14:12:47 -0300

    finish basic application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,36 +4,59 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
-
 import Head from "next/head";
-
 import styles from "../styles/pages/Home.module.css";
+import { GetServerSideProps } from "next";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
-
-export default function Home() {
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
 
-      <Head>
-        <title>Move.it</title>
-      </Head>
+        <Head>
+          <title>Move.it</title>
+        </Head>
 
-      <ExperienceBar />
+        <ExperienceBar />
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
 
-    </div>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    }
+  }
 }
